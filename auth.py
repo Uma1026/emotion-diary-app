0b2d3a6eb71d8c158e39c901df3442f0896a1e43
Uma1026 <umachekka26@gmail.com>
2026-04-04 19:41:26 +0530

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,51 @@
+import sqlite3
+
+def create_user_table():
+    conn = sqlite3.connect("emotion_diary.db")
+    c = conn.cursor()
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        age INTEGER,
+        gender TEXT,
+        address TEXT,
+        email TEXT UNIQUE,
+        password TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+def register_user(name, age, gender, address, email, password):
+    conn = sqlite3.connect("emotion_diary.db")
+    c = conn.cursor()
+
+    try:
+        c.execute("""
+        INSERT INTO users (name, age, gender, address, email, password)
+        VALUES (?, ?, ?, ?, ?, ?)
+        """, (name, age, gender, address, email, password))
+
+        conn.commit()
+        return True
+    except Exception as e:
+        print("Error:", e)  
+        return False
+    finally:
+        conn.close()
+
+
+def login_user(email, password):
+    conn = sqlite3.connect("emotion_diary.db")
+    c = conn.cursor()
+
+    c.execute("SELECT * FROM users WHERE email=? AND password=?", (email, password))
+
+    user = c.fetchone()
+    conn.close()
+
+    return user
